4713df8b77f5e8d5d604eba1a55871126164dd66
Mathias Ver Elst <mathias.ver.elst@gmail.com>
2026-07-16 01:02:22 +0200

    Fix bug setting the customerId instead of the companyName

--- src/pages/SingleTimesheetEntry.tsx
+++ src/pages/SingleTimesheetEntry.tsx
@@ -238,7 +238,7 @@ function SingleTimesheetEntry() {
                 <select disabled={isAccepted}
                         className="w-full bg-input-background text-foreground text-sm rounded-md px-3 py-2 border border-border focus:outline-none focus:ring-1 focus:ring-ring appearance-none cursor-pointer"
                         value={timesheetEntry.customerId} onChange={(e) => dispatch(updateCustomerId(e.target.value))}>
-                    {customers.map((customer) => <option key={customer.id}>{customer.companyName}</option>)}
+                    {customers.map((customer) => <option key={customer.id} value={customer.id}>{customer.companyName}</option>)}
                 </select>
             </div>
 
